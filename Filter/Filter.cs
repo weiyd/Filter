5608f32374e05c9579d5a5776c3c8207f838b04c
weiyd <727101923@qq.com>
2017-03-23 23:04:55 +0800

    添加注释

---
+++ Filter/Filter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter
+{
+    class Filter
+    {
+        // @param b 分子系数
+        // @param a 分母系数
+        // @param m 分子多项式阶数
+        // @param n 分母多项式阶数
+        // @param x 开始存放滤波器输入序列  后面存放滤波器输出序列
+        // @param len 输入序列与输出序列的长度
+        // @param px 保存前一个滤波器的m+1个输入值
+        // @param py 保存前一个滤波器的n个输出值
+        public void MyFilter(double[]b,double[]a,int m,int n,double x,int len,double[]px,double[]py)
+        {
+
+        }
+    }
+}
